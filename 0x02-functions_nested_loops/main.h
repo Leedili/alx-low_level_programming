a7f751ec7c5cefe5c407339507475ddd74560c15
Leedili <emilywaeni01@gmail.com>
2023-04-26 14:20:51 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+ void print_alphabet(void);
+  2 void print_alphabet_x10(void);
+  3 int _putchar(int c);
+  4 int _islower(int c);
+  5 int _isalpha(int c);
+  6 int print_sign(int n);
+  7 int _abs(int);
+  8 int print_last_digit(int);
+  9 void jack_bauer(void);
+ 10 void times_table(void);
+ 11 int add(int, int);
+ 12 void print_to_98(int n);
+ 13 void print_times_table(int n);
